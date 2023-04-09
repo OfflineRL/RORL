dfe0e0e6b0f8324642f811cc5847b282fe836ade
Thanh Nguyen <thanhkaist@github.com>
2023-04-09 15:13:48 +0900

    Integrate MIMI and BatchEnsemble

--- experiment_configs/configs/q_learning/sac_config.py
+++ experiment_configs/configs/q_learning/sac_config.py
@@ -1,7 +1,8 @@
 from lifelong_rl.models.networks import ParallelizedEnsembleFlattenMLP
+from lifelong_rl.models import networks
 from lifelong_rl.policies.base.base import MakeDeterministic
 from lifelong_rl.policies.models.tanh_gaussian_policy import TanhGaussianPolicy
-from lifelong_rl.trainers.q_learning.sac import SACTrainer
+from lifelong_rl.trainers.q_learning import sac
 import lifelong_rl.util.pythonplusplus as ppp
 import os
 import torch
@@ -16,11 +17,12 @@ def get_config(
         obs_dim,
         action_dim,
         replay_buffer,
+        efficient="MIMO",
 ):
     """
     Policy construction
     """
-
+    assert efficient in ["ENSAMBLE", "MIMO", "BATCH_ENSEMBLE"]
     num_qs = variant['trainer_kwargs']['num_qs']
     M = variant['policy_kwargs']['layer_size']
     num_q_layers = variant['policy_kwargs']['num_q_layers']
@@ -28,20 +30,8 @@ def get_config(
 
     # normalization
     norm_input = variant['norm_input']
-    obs_norm_mean, obs_norm_std = variant['normalization_info']['obs_mean'], variant['normalization_info']['obs_std']
-
-    qfs, target_qfs = ppp.group_init(
-        2,
-        ParallelizedEnsembleFlattenMLP,
-        ensemble_size=num_qs,
-        hidden_sizes=[M] * num_q_layers,
-        input_size=obs_dim + action_dim,
-        output_size=1,
-        layer_norm=None,
-        norm_input=norm_input,
-        obs_norm_mean=obs_norm_mean,
-        obs_norm_std=obs_norm_std,
-    )
+    obs_norm_mean, obs_norm_std = variant['normalization_info'][
+        'obs_mean'], variant['normalization_info']['obs_std']
 
     policy = TanhGaussianPolicy(
         obs_dim=obs_dim,
@@ -53,16 +43,82 @@ def get_config(
         obs_norm_std=obs_norm_std,
     )
 
-    trainer = SACTrainer(
-        env=eval_env,
-        policy=policy,
-        qfs=qfs,
-        target_qfs=target_qfs,
-        replay_buffer=replay_buffer,
-        norm_input=norm_input,
-        obs_std=obs_norm_std,
-        **variant['trainer_kwargs'],
-    )
+    if efficient == "ENSAMBLE":
+        qfs, target_qfs = ppp.group_init(
+            2,
+            ParallelizedEnsembleFlattenMLP,
+            ensemble_size=num_qs,
+            hidden_sizes=[M] * num_q_layers,
+            input_size=obs_dim + action_dim,
+            output_size=1,
+            layer_norm=None,
+            norm_input=norm_input,
+            obs_norm_mean=obs_norm_mean,
+            obs_norm_std=obs_norm_std,
+        )
+
+        trainer = sac.SACTrainer(
+            env=eval_env,
+            policy=policy,
+            qfs=qfs,
+            target_qfs=target_qfs,
+            replay_buffer=replay_buffer,
+            norm_input=norm_input,
+            obs_std=obs_norm_std,
+            **variant['trainer_kwargs'],
+        )
+
+    elif efficient == "MIMO":
+        qfs, target_qfs = ppp.group_init(
+            2,
+            networks.BatchEnsembleFlattenMLP,
+            ensemble_size=num_qs,
+            hidden_sizes=[M] * num_q_layers,
+            input_size=obs_dim + action_dim,
+            output_size=1,
+            layer_norm=None,
+            norm_input=norm_input,
+            obs_norm_mean=obs_norm_mean,
+            obs_norm_std=obs_norm_std,
+        )
+
+        trainer = sac.SACTrainerMimo(
+            env=eval_env,
+            policy=policy,
+            qfs=qfs,
+            target_qfs=target_qfs,
+            replay_buffer=replay_buffer,
+            norm_input=norm_input,
+            obs_std=obs_norm_std,
+            **variant['trainer_kwargs'],
+        )
+
+    elif efficient == "BATCH_ENSEMBLE":
+        qfs, target_qfs = ppp.group_init(
+            2,
+            networks.BatchEnsembleFlattenMLP,
+            ensemble_size=num_qs,
+            hidden_sizes=[M] * num_q_layers,
+            input_size=obs_dim + action_dim,
+            output_size=1,
+            layer_norm=None,
+            norm_input=norm_input,
+            obs_norm_mean=obs_norm_mean,
+            obs_norm_std=obs_norm_std,
+        )
+
+        trainer = sac.SACTrainerBatchEnsemble(
+            env=eval_env,
+            policy=policy,
+            qfs=qfs,
+            target_qfs=target_qfs,
+            replay_buffer=replay_buffer,
+            norm_input=norm_input,
+            obs_std=obs_norm_std,
+            **variant['trainer_kwargs'],
+        )
+    else:
+        raise NotImplementedError
 
     if variant['load_path'] != '':
         trainer.load_snapshot(variant['load_path'])
