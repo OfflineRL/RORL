c25d467872c318c024865061d98079538337b298
Thanh Nguyen <thanhkaist@github.com>
2023-04-22 20:26:33 +0900

    Add convenience files

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-# CUDA_VISIBLE_DEVICES=$1 python -m scripts.sac --env_name halfcheetah-medium-v2 --num_qs $3 \
-#     --norm_input --load_config_type benchmark --epoch 1000\
-#     --exp_prefix RORL_RANK1OPV1_BS$2_RELU --batch_size=$2
+CUDA_VISIBLE_DEVICES=$1 python -m scripts.sac --env_name hopper-medium-expert-v2 \
+    --norm_input --load_config_type benchmark --epoch 1000 --num_qs $2\
+    --exp_prefix RORL_RANK1GAUSS_HOPPER --tensorboard 
+ 
 
-CUDA_VISIBLE_DEVICES=$1 python -m scripts.sac --env_name halfcheetah-expert-v2 \
-    --norm_input --load_config_type benchmark \
-    --exp_prefix RORL_OPTUNE --tensorboard
+# CUDA_VISIBLE_DEVICES=$1 python -m scripts.sac --env_name halfcheetah-expert-v2 \
+#     --norm_input --load_config_type benchmark \
+#     --exp_prefix RORL_OPTUNE --tuning --epoch 10
  
