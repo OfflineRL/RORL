d6db6fc7498670af07c51975bd6e83e8e573a60f
Thanh Nguyen <thanhkaist@github.com>
2023-04-25 17:33:14 +0900

    Update script

--- tune.sh
+++ tune.sh
@@ -20,15 +20,16 @@
 # bash tune.sh 7 walker2d-medium-replay-v2 14
 
 # NOW: bash tune.sh hopper-medium-expert-v2 hopper_medium_expert 1
+# bash tune.sh halfcheetah-medium-v2  halfcheetah_medium 1
 
 WANDB_RUN_GROUP=$2 CUDA_VISIBLE_DEVICES=$3 python -m scripts.sac --env_name $1 --seed 0 \
-    --norm_input --epoch 1000 --exp_prefix RORL_TUNE_GAUSS  --tensorboard --wandb \
-    --tuning --policy_smooth_eps 0.005 --q_smooth_eps 0.005
+    --norm_input --epoch 1000 --exp_prefix RORL_TUNE_GAUSS_TEST  --tensorboard --wandb \
+    --policy_smooth_eps 0.005 --q_smooth_eps 0.005 \
+    --q_ood_uncertainty_reg 1 --q_ood_eps 0.01 --q_ood_reg 0.5 --tuning 
 
 
-
-
-# CUDA_VISIBLE_DEVICES=$1 python -m scripts.sac --env_name halfcheetah-expert-v2 \
-#     --norm_input --load_config_type benchmark \
-#     --exp_prefix RORL_OPTUNE --tuning --epoch 10
+# WANDB_RUN_GROUP=$2 CUDA_VISIBLE_DEVICES=$3 python -m scripts.sac --env_name $1 --seed 0 \
+#     --norm_input --epoch 1000 --exp_prefix RORL_TUNE_GAUSS_TEST  --tensorboard --wandb \
+#     --policy_smooth_eps 0.005 --q_smooth_eps 0.005 \
+#     --q_ood_uncertainty_reg 1 --q_ood_eps 0.01 --q_ood_reg 0.5 #--tuning 
  
